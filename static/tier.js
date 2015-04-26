7a501461d86c272c19d91753a25ea9968d34e822
prest255 <prest255@umn.edu>
2015-04-26 17:46:10 -0500

    Added Diamond III

--- static/tier.js
+++ static/tier.js
@@ -1,7 +1,7 @@
 var tiers = [
     {id: 1, image:"CHALLENGER_I", name:"Challenger"},
     {id: 2, image:"MASTER_I", name:"Master"},
-    {id: 3, image:"DIAMOND_I", name:"Diamond I"}, {id:4, image:"DIAMOND_II", name:"Diamond II"}, {id:5, image:"DIAMOND_III", name:"Diamond II"}, {id:6, image:"DIAMOND_IV", name:"Diamond IV"}, {id:7, image:"DIAMOND_V", name:"Diamond V"},
+    {id: 3, image:"DIAMOND_I", name:"Diamond I"}, {id:4, image:"DIAMOND_II", name:"Diamond II"}, {id:5, image:"DIAMOND_III", name:"Diamond III"}, {id:6, image:"DIAMOND_IV", name:"Diamond IV"}, {id:7, image:"DIAMOND_V", name:"Diamond V"},
     {id: 8, image:"PLATINUM_I", name:"Platinum I"}, {id: 9, image:"PLATINUM_II", name:"Platinum II"}, {id: 10, image:"PLATINUM_III", name:"Platinum III"}, {id:11, image:"PLATINUM_IV", name:"Platinum IV"}, {id: 12, image:"PLATINUM_V", name:"Platinum V"},
     {id:13, image:"GOLD_I", name:"Gold I"}, {id:14, image:"GOLD_II", name:"Gold II"}, {id:15, image:"GOLD_III", name:"Gold III"}, {id: 16, image:"GOLD_IV", name:"Gold IV"}, {id:17, image:"GOLD_V", name:"Gold V"},
     {id:18, image:"SILVER_I", name:"Silver I"}, {id:19, image:"SILVER_II", name:"Silver II"}, {id:20, image:"SILVER_III", name:"Silver III"}, {id:21, image:"SILVER_IV", name:"Silver IV"}, {id:22, image:"SILVER_V", name:"Silver V"},
